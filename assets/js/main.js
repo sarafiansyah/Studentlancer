8464f1a7963aa09cd71a494337a000e7ca0d2cd9
Mahesa Rafian Syah <mahesarafiansyah@gmail.com>
2023-03-29 11:50:13 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,18 @@
-$(document).ready(function(){
-    $(".testimonial .indicators li").click(function(){
-      var i = $(this).index();
-      var targetElement = $(".testimonial .tabs li");
-      targetElement.eq(i).addClass('active');
-      targetElement.not(targetElement[i]).removeClass('active');
-              });
-              $(".testimonial .tabs li").click(function(){
-                  var targetElement = $(".testimonial .tabs li");
-                  targetElement.addClass('active');
-                  targetElement.not($(this)).removeClass('active');
-              });
-          });
-  $(document).ready(function(){
-      $(".slider .swiper-pagination span").each(function(i){
-          $(this).text(i+1).prepend("0");
-      });
-  });
-  document.onreadystatechange = function() {
-    let lastScrollPosition = 0;
-    const navbar = document.querySelector('.navbar');
-    window.addEventListener('scroll', function(e) {
-      lastScrollPosition = window.scrollY;
-      
-      if (lastScrollPosition > 100)
-        navbar.classList.add('navbar-dark');
-      else
-        navbar.classList.remove('navbar-dark');
+$(document).ready(function () {
+    $(".testimonial .indicators li").click(function () {
+        var i = $(this).index();
+        var targetElement = $(".testimonial .tabs li");
+        targetElement.eq(i).addClass('active');
+        targetElement.not(targetElement[i]).removeClass('active');
     });
-  }
+    $(".testimonial .tabs li").click(function () {
+        var targetElement = $(".testimonial .tabs li");
+        targetElement.addClass('active');
+        targetElement.not($(this)).removeClass('active');
+    });
+});
+$(document).ready(function () {
+    $(".slider .swiper-pagination span").each(function (i) {
+        $(this).text(i + 1).prepend("0");
+    });
+});
