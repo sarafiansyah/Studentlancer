2a06df98751b63fbe756d9ffbf4c60b39edb4067
Mahesa Rafian Syah <mahesarafiansyah@gmail.com>
2023-06-30 19:29:42 +0700

    Reupdate base code

---
+++ About.php
@@ -0,0 +1,299 @@
+ <section id="content">
+ 	<div class="container" style="margin-left: auto; margin-right: auto; text-align: center; justify-content: center;">
+ 		<div class="row justify-content-center mx-auto ">
+ 			<div class="col-12 col-sm-8 col-lg-6">
+ 				<!-- Section Heading-->
+ 				<div class="section_heading text-center wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+ 					<h3>Our Creative <span> Team</span></h3>
+ 					<p>Appland is completely creative, lightweight, clean &amp; super responsive app landing page.</p>
+ 					<div class="line"></div>
+ 				</div>
+ 			</div>
+ 		</div>
+ 		<div class="row justify-content-center mx-auto text-center" style="margin-left: auto; margin-right: auto; text-align: center; justify-content: center;">
+ 			<!-- Single Advisor-->
+ 			<div class="col-12 col-sm-6 col-lg-3">
+ 				<div class="single_advisor_profile wow fadeInUp " data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp; box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;">
+ 					<!-- Team Thumb-->
+ 					<div class="advisor_thumb">
+ 						<img src="plugins/home-plugins/img/devs/dev02.png" height=300 width=220 style="margin-left: 0px;" alt="">
+ 						<!-- Social Info-->
+ 						<div class="social-info"><a href="#"><i class="fa-brands fa-facebook-f"></i></i></a><a href="#"><i class="fa-brands fa-twitter"></i></a><a href="#"><i class="fa-brands fa-instagram"></i></a><a href="#"><i class="fa-brands fa-linkedin"></i></a></div>
+ 					</div>
+ 					<!-- Team Details-->
+ 					<div class="single_advisor_details_info">
+ 						<h6><b>Mahesa Rafiansyah</b></h6>
+ 						<p class="designation">Chief Technology Officer</p>
+ 					</div>
+ 				</div>
+ 			</div>
+ 			<!-- Single Advisor-->
+ 			<div class="col-12 col-sm-6 col-lg-3">
+ 				<div class="single_advisor_profile wow fadeInUp " data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp; box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;">
+ 					<!-- Team Thumb-->
+ 					<div class="advisor_thumb">
+ 						<img src="plugins/home-plugins/img/devs/dev01.png" height=300 width=170 style="margin-left: 10px;" alt="">
+ 						<!-- Social Info-->
+ 						<div class="social-info"><a href="#"><i class="fa-brands fa-facebook-f"></i></i></a><a href="#"><i class="fa-brands fa-twitter"></i></a><a href="#"><i class="fa-brands fa-instagram"></i></a><a href="#"><i class="fa-brands fa-linkedin"></i></a></div>
+ 					</div>
+ 					<!-- Team Details-->
+ 					<div class="single_advisor_details_info">
+ 						<h6><b>Cristha Patrisya Pentury</b></h6>
+ 						<p class="designation">Chief Executive Officer</p>
+ 					</div>
+ 				</div>
+ 			</div>
+ 			<!-- Single Advisor-->
+
+ 			<div class="col-12 col-sm-6 col-lg-3">
+ 				<div class="single_advisor_profile wow fadeInUp " data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp; box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;">
+ 					<!-- Team Thumb-->
+ 					<div class="advisor_thumb">
+ 						<img src="plugins/home-plugins/img/devs/dev03.png" height=300 width=230 style="margin-left: -10px;" alt="">
+ 						<!-- Social Info-->
+ 						<div class="social-info"><a href="#"><i class="fa-brands fa-facebook-f"></i></i></a><a href="#"><i class="fa-brands fa-twitter"></i></a><a href="#"><i class="fa-brands fa-instagram"></i></a><a href="#"><i class="fa-brands fa-linkedin"></i></a></div>
+ 					</div>
+ 					<!-- Team Details-->
+ 					<div class="single_advisor_details_info">
+ 						<h6><b>Yohanes Stefanus</b></h6>
+ 						<p class="designation">Chief Marketing Officer</p>
+ 					</div>
+ 				</div>
+ 			</div>
+ 			<!-- Single Advisor-->
+ 		</div>
+ 		<div class="row justify-content-center mx-auto text-center" style="margin-left: auto; margin-right: auto; text-align: center; justify-content: center;">
+ 			<!-- Single Advisor-->
+ 			<div class="col-12 col-sm-6 col-lg-3">
+ 				<div class="single_advisor_profile wow fadeInUp " data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp; box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;">
+ 					<!-- Team Thumb-->
+ 					<div class="advisor_thumb">
+ 						<img src="plugins/home-plugins/img/devs/dev02.png" height=300 width=220 style="margin-left: 0px;" alt="">
+ 						<!-- Social Info-->
+ 						<div class="social-info"><a href="#"><i class="fa-brands fa-facebook-f"></i></i></a><a href="#"><i class="fa-brands fa-twitter"></i></a><a href="#"><i class="fa-brands fa-instagram"></i></a><a href="#"><i class="fa-brands fa-linkedin"></i></a></div>
+ 					</div>
+ 					<!-- Team Details-->
+ 					<div class="single_advisor_details_info">
+ 						<h6><b>Mahesa Rafiansyah</b></h6>
+ 						<p class="designation">Chief Technology Officer</p>
+ 					</div>
+ 				</div>
+ 			</div>
+
+ 			<!-- Single Advisor-->
+
+ 			<div class="col-12 col-sm-6 col-lg-3">
+ 				<div class="single_advisor_profile wow fadeInUp " data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp; box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;">
+ 					<!-- Team Thumb-->
+ 					<div class="advisor_thumb">
+ 						<img src="plugins/home-plugins/img/devs/dev03.png" height=300 width=230 style="margin-left: -10px;" alt="">
+ 						<!-- Social Info-->
+ 						<div class="social-info"><a href="#"><i class="fa-brands fa-facebook-f"></i></i></a><a href="#"><i class="fa-brands fa-twitter"></i></a><a href="#"><i class="fa-brands fa-instagram"></i></a><a href="#"><i class="fa-brands fa-linkedin"></i></a></div>
+ 					</div>
+ 					<!-- Team Details-->
+ 					<div class="single_advisor_details_info">
+ 						<h6><b>Yohanes Stefanus</b></h6>
+ 						<p class="designation">Chief Marketing Officer</p>
+ 					</div>
+ 				</div>
+ 			</div>
+ 			<!-- Single Advisor-->
+ 		</div>
+ 	</div>
+ 	<section class="section-padding">
+ 		<div class="container">
+ 			<div class="row showcase-section">
+ 				<div class="col-md-6">
+ 					<img src="img/dev1.png" alt="showcase image">
+ 				</div>
+ 				<div class="col-md-6">
+ 					<div class="about-text">
+ 						<h3>Lorem Ipsum Dolor sit</h3>
+ 						<p>Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Vivamus suscipit tortor eget felis porttitor volutpat. Cras ultricies ligula sed magna dictum porta. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar.</p>
+ 						<p>Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo..</p>
+ 					</div>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</section>
+ 	<div class="container">
+
+ 		<div class="about">
+
+
+ 			<div class="row">
+ 				<div class="col-md-4">
+ 					<!-- Heading and para -->
+ 					<div class="block-heading-two">
+ 						<h3><span>Why Choose Us?</span></h3>
+ 					</div>
+ 					<p>Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur. <br /><br />Sed ut perspiciaatis iste natus error sit voluptatem probably haven't heard of them accusamus.</p>
+ 				</div>
+ 				<div class="col-md-4">
+ 					<div class="block-heading-two">
+ 						<h3><span>Our Solution</span></h3>
+ 					</div>
+ 					<!-- Accordion starts -->
+ 					<div class="panel-group" id="accordion-alt3">
+ 						<!-- Panel. Use "panel-XXX" class for different colors. Replace "XXX" with color. -->
+ 						<div class="panel">
+ 							<!-- Panel heading -->
+ 							<div class="panel-heading">
+ 								<h4 class="panel-title">
+ 									<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
+ 										<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 1
+ 									</a>
+ 								</h4>
+ 							</div>
+ 							<div id="collapseOne-alt3" class="panel-collapse collapse">
+ 								<!-- Panel body -->
+ 								<div class="panel-body">
+ 									Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+ 								</div>
+ 							</div>
+ 						</div>
+ 						<div class="panel">
+ 							<div class="panel-heading">
+ 								<h4 class="panel-title">
+ 									<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3">
+ 										<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 2
+ 									</a>
+ 								</h4>
+ 							</div>
+ 							<div id="collapseTwo-alt3" class="panel-collapse collapse">
+ 								<div class="panel-body">
+ 									Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+ 								</div>
+ 							</div>
+ 						</div>
+ 						<div class="panel">
+ 							<div class="panel-heading">
+ 								<h4 class="panel-title">
+ 									<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3">
+ 										<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 3
+ 									</a>
+ 								</h4>
+ 							</div>
+ 							<div id="collapseThree-alt3" class="panel-collapse collapse">
+ 								<div class="panel-body">
+ 									Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+ 								</div>
+ 							</div>
+ 						</div>
+ 						<div class="panel">
+ 							<div class="panel-heading">
+ 								<h4 class="panel-title">
+ 									<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFour-alt3">
+ 										<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 4
+ 									</a>
+ 								</h4>
+ 							</div>
+ 							<div id="collapseFour-alt3" class="panel-collapse collapse">
+ 								<div class="panel-body">
+ 									Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+ 								</div>
+ 							</div>
+ 						</div>
+ 					</div>
+ 					<!-- Accordion ends -->
+
+ 				</div>
+
+ 				<div class="col-md-4">
+ 					<div class="block-heading-two">
+ 						<h3><span>Our Expertise</span></h3>
+ 					</div>
+ 					<h6>Web Development</h6>
+ 					<div class="progress pb-sm">
+ 						<!-- White color (progress-bar-white) -->
+ 						<div class="progress-bar progress-bar-red" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+ 							<span class="sr-only">40% Complete (success)</span>
+ 						</div>
+ 					</div>
+ 					<h6>Designing</h6>
+ 					<div class="progress pb-sm">
+ 						<div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+ 							<span class="sr-only">40% Complete (success)</span>
+ 						</div>
+ 					</div>
+ 					<h6>User Experience</h6>
+ 					<div class="progress pb-sm">
+ 						<div class="progress-bar progress-bar-lblue" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+ 							<span class="sr-only">40% Complete (success)</span>
+ 						</div>
+ 					</div>
+ 					<h6>Development</h6>
+ 					<div class="progress pb-sm">
+ 						<div class="progress-bar progress-bar-yellow" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%">
+ 							<span class="sr-only">40% Complete (success)</span>
+ 						</div>
+ 					</div>
+ 				</div>
+
+ 			</div>
+
+
+
+ 			<br>
+ 			<!-- Our Team starts -->
+
+ 			<!-- Heading -->
+ 			<div class="block-heading-six">
+ 				<h4 class="bg-color">Our Team</h4>
+ 			</div>
+ 			<br>
+
+ 			<!-- Our team starts -->
+
+ 			<div class="team-six">
+ 				<div class="row">
+ 					<div class="col-md-3 col-sm-6">
+ 						<!-- Team Member -->
+ 						<div class="team-member">
+ 							<!-- Image -->
+ 							<img class="img-responsive" src="img/team1.jpg" alt="">
+ 							<!-- Name -->
+ 							<h4>Johne Doe</h4>
+ 							<span class="deg">Creative</span>
+ 						</div>
+ 					</div>
+ 					<div class="col-md-3 col-sm-6">
+ 						<!-- Team Member -->
+ 						<div class="team-member">
+ 							<!-- Image -->
+ 							<img class="img-responsive" src="img/team2.jpg" alt="">
+ 							<!-- Name -->
+ 							<h4>Jennifer</h4>
+ 							<span class="deg">Programmer</span>
+ 						</div>
+ 					</div>
+ 					<div class="col-md-3 col-sm-6">
+ 						<!-- Team Member -->
+ 						<div class="team-member">
+ 							<!-- Image -->
+ 							<img class="img-responsive" src="img/team3.jpg" alt="">
+ 							<!-- Name -->
+ 							<h4>Christean</h4>
+ 							<span class="deg">CEO</span>
+ 						</div>
+ 					</div>
+ 					<div class="col-md-3 col-sm-6">
+ 						<!-- Team Member -->
+ 						<div class="team-member">
+ 							<!-- Image -->
+ 							<img class="img-responsive" src="img/team4.jpg" alt="">
+ 							<!-- Name -->
+ 							<h4>Kerinele rase</h4>
+ 							<span class="deg">Manager</span>
+ 						</div>
+ 					</div>
+ 				</div>
+ 			</div>
+
+ 			<!-- Our team ends -->
+
+
+ 		</div>
+
+ 	</div>
+ </section>
