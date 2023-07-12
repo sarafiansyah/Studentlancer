06b6e3bcbe46d1133e60eac368390c5618f64440
Mahesa Rafian Syah <mahesarafiansyah@gmail.com>
2023-07-13 03:47:15 +0700

    Update db_stdlancer.sql

--- db_stdlancer.sql
+++ db_stdlancer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 07, 2023 at 12:04 PM
+-- Generation Time: Jul 12, 2023 at 10:37 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -55,7 +55,8 @@ INSERT INTO `tblapplicants` (`APPLICANTID`, `FNAME`, `LNAME`, `MNAME`, `ADDRESS`
 (2018013, 'Kim', 'Domingo', 'Enoe', 'Kab City', 'Female', 'none', '1991-01-01', 'Kab Citys', 27, 'kim', 'a6312121e15caec74845b7ba5af23330d52d4ac0', 'kim@y.com', '5415456', 'BSAC', 'photos/RobloxScreenShot20180406_203758793.png', ''),
 (2018014, 'Jake', 'Zyrus', 'Ilmba', 'Kab City', 'Female', 'none', '1993-01-16', 'Kab City', 25, 'jake', 'c8d99c2f7cd5f432c163abcd422672b9f77550bb', 'jake@y.com', '14655623123123', 'BSIT', '', ''),
 (2018015, 'Janry', 'Tan', 'Lim', 'brgy 1 Kab City', 'Female', 'Single', '1992-01-30', 'Kab City', 26, 'janry', '1dd4efc811372cd1efe855981a8863d10ddde1ca', 'jan@gmail.com', '0234234', 'BSIT', '', ''),
-(2023016, 'Cristha', 'Pentury', 'Patrisya', '123', 'Female', 'Single', '1987-06-26', 'Jakarta', 35, 'peped', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'peped@gmail.com', '123', 'Ha?', 'photos/photo3jpg.jpg', '');
+(2023016, 'Cristha', 'Peped Pentury', 'Patrisya', '123', 'Female', 'Single', '1987-06-26', 'Jakarta', 36, 'peped', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'peped@gmail.com', '123', 'Ha?', 'photos/photo3jpg.jpg', ''),
+(2023017, 'Mahesa', 'Syah', 'Rafian', 'Jakarta', 'Male', 'Single', '2002-05-03', 'Jakarta', 21, 'mahesa', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'mahesa@gmail.com', '0811111111', '', 'photos/20210327_010021.jpg', '');
 
 -- --------------------------------------------------------
 
@@ -80,7 +81,11 @@ INSERT INTO `tblattachmentfile` (`ID`, `FILEID`, `JOBID`, `FILE_NAME`, `FILE_LOC
 (2, '2147483647', 2, 'Resume', 'photos/27052018124027PLATENO FE95483.docx', 2018013),
 (3, '20236912529', 2, 'Resume', 'photos/26042023072349Sertif  (3).png', 2023016),
 (4, '20236912530', 3, 'Resume', 'photos/11062023031913CV_MahesaRafianSyah_2023_rev2102 (2).pdf', 2023016),
-(5, '20236912531', 4, 'Resume', 'photos/22062023063558412020016_mahesarafiansyah.py', 2023016);
+(5, '20236912531', 4, 'Resume', 'photos/22062023063558412020016_mahesarafiansyah.py', 2023016),
+(6, '20236912533', 9, 'Resume', 'photos/120720230716581x1.png', 2023016),
+(7, '20236912534', 9, 'Resume', 'photos/120720230724261x1.pdf', 2023016),
+(8, '20236912535', 10, 'Resume', 'photos/120720230726391x1.png', 2023016),
+(9, '20236912536', 11, 'Resume', 'photos/12072023082423Untitled design.pdf', 2023017);
 
 -- --------------------------------------------------------
 
@@ -103,8 +108,8 @@ CREATE TABLE `tblautonumbers` (
 INSERT INTO `tblautonumbers` (`AUTOID`, `AUTOSTART`, `AUTOEND`, `AUTOINC`, `AUTOKEY`) VALUES
 (1, '02983', 7, 1, 'userid'),
 (2, '000', 78, 1, 'employeeid'),
-(3, '0', 17, 1, 'APPLICANT'),
-(4, '69125', 32, 1, 'FILEID');
+(3, '0', 18, 1, 'APPLICANT'),
+(4, '69125', 37, 1, 'FILEID');
 
 -- --------------------------------------------------------
 
@@ -155,11 +160,11 @@ CREATE TABLE `tblcompany` (
 --
 
 INSERT INTO `tblcompany` (`COMPANYID`, `COMPANYNAME`, `COMPANYADDRESS`, `COMPANYCONTACTNO`, `COMPANYSTATUS`, `COMPANYMISSION`) VALUES
-(2, 'URC', 'Bry Camugao', '023654', '', 'weqwe'),
-(3, 'Copreros', 'Mabinay\'s', '035656', '', ''),
-(4, 'Quest', 'Kabankalan City', '23165', '', ''),
-(6, 'Palacios Company', 'Kabankalan City', '0625656899', '', ''),
-(7, 'IT Company', 'Kabankalan City', '04564123', '', '');
+(2, 'Sinarmas', 'Jakarta', '023654', '', 'weqwe'),
+(3, 'Binus University', 'Jakarta', '035656', '', ''),
+(4, 'Universitas Trisakti', 'Jakarta', '23165', '', ''),
+(6, 'Universitas Tarumanagara', 'Jakarta', '0625656899', '', ''),
+(7, 'Universitas Kristen Krida Wacana', 'Jakarta', '04564123', '', '');
 
 -- --------------------------------------------------------
 
@@ -218,7 +223,9 @@ CREATE TABLE `tblfeedback` (
 INSERT INTO `tblfeedback` (`FEEDBACKID`, `APPLICANTID`, `REGISTRATIONID`, `FEEDBACK`) VALUES
 (2, 2023016, 3, 'Good to Go!'),
 (3, 2023016, 4, 'Accepted, please contact me at 081211223344'),
-(4, 2023016, 5, 'Selamat kamu diterima');
+(4, 2023016, 5, 'Selamat kamu diterima'),
+(5, 2023016, 8, 'Accepted, Please Contact 111111'),
+(6, 2023017, 9, 'Accepter, Please Contanct 0811111111');
 
 -- --------------------------------------------------------
 
@@ -229,6 +236,7 @@ INSERT INTO `tblfeedback` (`FEEDBACKID`, `APPLICANTID`, `REGISTRATIONID`, `FEEDB
 CREATE TABLE `tbljob` (
   `JOBID` int(11) NOT NULL,
   `COMPANYID` int(11) NOT NULL,
+  `RECRUITERNAME` varchar(255) NOT NULL,
   `CATEGORY` varchar(250) NOT NULL,
   `OCCUPATIONTITLE` varchar(90) NOT NULL,
   `REQ_NO_EMPLOYEES` int(11) NOT NULL,
@@ -246,11 +254,17 @@ CREATE TABLE `tbljob` (
 -- Dumping data for table `tbljob`
 --
 
-INSERT INTO `tbljob` (`JOBID`, `COMPANYID`, `CATEGORY`, `OCCUPATIONTITLE`, `REQ_NO_EMPLOYEES`, `SALARIES`, `DURATION_EMPLOYEMENT`, `QUALIFICATION_WORKEXPERIENCE`, `JOBDESCRIPTION`, `PREFEREDSEX`, `SECTOR_VACANCY`, `JOBSTATUS`, `DATEPOSTED`) VALUES
-(1, 2, 'Technology', 'ISD', 6, 15000, 'jan 30', 'Two year Experience', 'We are looking for bachelor of science in information technology.\r\nasdasdasd', 'Male/Female', 'yes', '', '2018-05-20 00:00:00'),
-(2, 2, 'Technology', 'Accounting', 1, 15000, 'may 20', 'Two years Experience', 'We are looking for bachelor of science in Acountancy', 'Female', 'yes', '', '2018-05-20 02:33:00'),
-(3, 6, 'Technology', 'Create A Website', 1, 20000, '1 day', 'All majors', 'Please Make me a website', 'Male/Female', 'All', '', '2023-06-11 15:16:00'),
-(4, 7, 'Technology', 'Create Mobile App', 1, 100, '1 day', 'S1', 'Need S1', 'Male/Female', 'IT', '', '2023-06-22 06:35:00');
+INSERT INTO `tbljob` (`JOBID`, `COMPANYID`, `RECRUITERNAME`, `CATEGORY`, `OCCUPATIONTITLE`, `REQ_NO_EMPLOYEES`, `SALARIES`, `DURATION_EMPLOYEMENT`, `QUALIFICATION_WORKEXPERIENCE`, `JOBDESCRIPTION`, `PREFEREDSEX`, `SECTOR_VACANCY`, `JOBSTATUS`, `DATEPOSTED`) VALUES
+(2, 2, '', 'Technology', 'Accounting', 1, 15000, 'may 20', 'Two years Experience', 'We are looking for bachelor of science in Acountancy', 'Female', 'yes', '', '2018-05-20 02:33:00'),
+(3, 6, '', 'Technology', 'Create A Website', 1, 20000, '1 day', 'All majors', 'Please Make me a website', 'Male/Female', 'All', '', '2023-06-11 15:16:00'),
+(4, 7, '', 'Technology', 'Create Mobile App', 1, 100, '1 day', 'S1', 'Need S1', 'Male/Female', 'IT', '', '2023-06-22 06:35:00'),
+(5, 1, '', 'Technology', 'Test an application', 1, 20000, '1 day', 'Bebas', 'Ngapain kek gtu', 'Male/Female', 'Apa ajalah', '', '2023-07-12 18:40:00'),
+(6, 6, '', 'Managerial', 'Test an application', 1, 20000, '1 day', '', '', 'Male/Female', '', '', '2023-07-12 18:54:00'),
+(7, 7, '', 'Engineer', 'Make an engine', 1, 20000, '1 day', '', '', 'None', '', '', '2023-07-12 18:54:00'),
+(8, 7, 'Studentlancer Indonesia', 'IT', 'Play a game', 0, 0, '', '', '', 'None', '', '', '2023-07-12 19:06:00'),
+(10, 7, 'Studentlancer Indonesia', 'Engineer', 'make a rocket', 0, 0, '', '', '', 'None', '', '', '2023-07-12 19:25:00'),
+(11, 7, 'Studentlancer Indonesia', 'Technology', 'say Hello to Pak Aswin', 0, 0, '', '', '', 'None', '', '', '2023-07-12 19:47:00'),
+(12, 7, 'Mahesa Rafian Syah', 'IT', 'Just make something', 0, 0, '', '', '', 'None', '', '', '2023-07-12 21:44:00');
 
 -- --------------------------------------------------------
 
@@ -281,7 +295,11 @@ INSERT INTO `tbljobregistration` (`REGISTRATIONID`, `COMPANYID`, `JOBID`, `APPLI
 (2, 2, 2, 2018015, 'Janry Tan', '2018-05-26', 'aasd', '2147483647', 0, 0, '2018-05-28 14:14:45'),
 (3, 2, 2, 2023016, 'Cristha Pentury', '2023-04-26', 'Good to Go!', '20236912529', 0, 1, '2023-04-27 00:25:37'),
 (4, 6, 3, 2023016, 'Cristha Pentury', '2023-06-11', 'Accepted, please contact me at 081211223344', '20236912530', 0, 1, '2023-06-11 20:20:18'),
-(5, 7, 4, 2023016, 'Cristha Pentury', '2023-06-22', 'Selamat kamu diterima', '20236912531', 0, 1, '2023-06-22 11:36:46');
+(5, 7, 4, 2023016, 'Cristha Pentury', '2023-06-22', 'Selamat kamu diterima', '20236912531', 0, 1, '2023-06-22 11:36:46'),
+(6, 7, 9, 2023016, 'Cristha Peped Pentury', '2023-07-12', 'Pending', '20236912533', 1, 1, '2023-07-12 19:16:00'),
+(7, 7, 9, 2023016, 'Cristha Peped Pentury', '2023-07-12', 'Pending', '20236912534', 1, 1, '2023-07-12 19:24:00'),
+(8, 7, 10, 2023016, 'Cristha Peped Pentury', '2023-07-12', 'Accepted, Please Contact 111111', '20236912535', 0, 1, '2023-07-13 01:11:33'),
+(9, 7, 11, 2023017, 'Mahesa Syah', '2023-07-12', 'Accepter, Please Contanct 0811111111', '20236912536', 0, 1, '2023-07-13 01:25:08');
 
 -- --------------------------------------------------------
 
@@ -290,7 +308,7 @@ INSERT INTO `tbljobregistration` (`REGISTRATIONID`, `COMPANYID`, `JOBID`, `APPLI
 --
 
 CREATE TABLE `tblusers` (
-  `USERID` varchar(30) NOT NULL,
+  `USERID` int(11) NOT NULL,
   `FULLNAME` varchar(40) NOT NULL,
   `USERNAME` varchar(90) NOT NULL,
   `PASS` varchar(90) NOT NULL,
@@ -303,8 +321,12 @@ CREATE TABLE `tblusers` (
 --
 
 INSERT INTO `tblusers` (`USERID`, `FULLNAME`, `USERNAME`, `PASS`, `ROLE`, `PICLOCATION`) VALUES
-('00018', 'Campcodes', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Administrator', 'photos/Koala.jpg'),
-('2018001', 'Chambe Narciso', 'Narciso', 'f3593fd40c55c33d1788309d4137e82f5eab0dea', 'Employee', '');
+(1, 'Nico Sanjaya', 'nico', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'Administrator', '_'),
+(18, 'Studentlancer Indonesia', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Administrator', 'photos/Koala.jpg'),
+(19, 'Mahesa Rafian Syah', 'mahesa', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'Administrator', '_'),
+(2018001, 'Chambe Narciso', 'Narciso', 'f3593fd40c55c33d1788309d4137e82f5eab0dea', 'Employee', ''),
+(2018002, 'Diana Novita', 'diana', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'Administrator', 'photos/default.jpg'),
+(2018003, 'Yohanes', 'yohanes', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'Administrator', 'photos/default.jpg');
 
 --
 -- Indexes for dumped tables
@@ -379,13 +401,13 @@ ALTER TABLE `tblusers`
 -- AUTO_INCREMENT for table `tblapplicants`
 --
 ALTER TABLE `tblapplicants`
-  MODIFY `APPLICANTID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2023017;
+  MODIFY `APPLICANTID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2023018;
 
 --
 -- AUTO_INCREMENT for table `tblattachmentfile`
 --
 ALTER TABLE `tblattachmentfile`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tblautonumbers`
@@ -415,19 +437,25 @@ ALTER TABLE `tblemployees`
 -- AUTO_INCREMENT for table `tblfeedback`
 --
 ALTER TABLE `tblfeedback`
-  MODIFY `FEEDBACKID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `FEEDBACKID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tbljob`
 --
 ALTER TABLE `tbljob`
-  MODIFY `JOBID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `JOBID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `tbljobregistration`
 --
 ALTER TABLE `tbljobregistration`
-  MODIFY `REGISTRATIONID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `REGISTRATIONID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  MODIFY `USERID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2018004;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
